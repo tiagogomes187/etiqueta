f21fade06beff7a92353d99ad019b8641c0b84b7
Tiago Gomes <contato@tiagogomes.dev.br>
2025-09-24 20:49:09 -0300

    Backend project created

---
+++ backend/src/main/java/br/dev/tiagogomes/etiqueta/EtiquetaApplication.java
@@ -0,0 +1,13 @@
+package br.dev.tiagogomes.etiqueta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtiquetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtiquetaApplication.class, args);
+	}
+
+}
